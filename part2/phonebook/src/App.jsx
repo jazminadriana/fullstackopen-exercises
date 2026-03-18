1f158498466649d679a184d85a26dfc27ab6b24c
Jazmín Hernández <li.jhernandezz@gmail.com>
2026-03-18 17:50:57 -0600

    feat: complete exercise 2.8 phonebook step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,14 +3,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  // Function to handle the input change for the new name
+  // Functions to handle changes in the input fields for name and number
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
+
+  const handleNumberChange = (event) => {
+  setNewNumber(event.target.value)  
+}
 
   // Function to handle the form submission for adding a new person
   const addPerson = (event) => {
@@ -27,10 +32,12 @@ const App = () => {
 
     const personObject = {
       name: newName,
+      number: newNumber
     }
 
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -41,13 +48,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map(person => 
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{person.name} <span>{person.number}</span> </li>
         )}
       </ul>
     </div>
