1a502c5982a726c78b9b7303ade9242768bffdbd
Jazmín Hernández <li.jhernandezz@gmail.com>
2026-03-17 18:13:19 -0600

    feat: complet exercises 2.1 - 2.5

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,33 @@
+const Header = ({ courseName }) => <h2>{courseName}</h2>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((s, p) => s + p.exercises, 0)
+  return <strong>total of {total} exercises</strong>
+}
+
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
